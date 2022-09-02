6d72d956833d3274510a2d2b975234334738c564
MaheshChand1528 <mannu44800@gmail.com>
2022-09-02 21:00:46 +0530

    make it better

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ var app = express();
 app.get("/", (req, res) => {
   res.send("server start");
 });
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 app.use(
   cors({
     origin: "*",
@@ -32,7 +20,7 @@ app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const DB = process.env.DATABASE;
 
 mongoose
