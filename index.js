393544171234f17feb98508a7923d50f4b166791
MaheshChand1528 <mannu44800@gmail.com>
2022-09-02 16:14:09 +0530

    make it better

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ var app = express();
 app.get("/", (req, res) => {
   res.send("server start");
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors({
   origin:"*",
   methods:['GET','POST','PUT','DELETE'],
