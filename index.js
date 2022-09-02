6fa2f382f54d592800f854d8b07c6c56cdfc53da
MaheshChand1528 <mannu44800@gmail.com>
2022-09-02 22:01:14 +0530

    make it better

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 app.get("/", (req, res) => {
   res.send("server start");
 });
-// app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -21,21 +21,21 @@ app.use((req, res, next) => {
   }
   next();
 });
-module.exports.cors = {
-  allRoutes: true,
-  origin: '*',
-  credentials: true,
-  methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
-  headers: 'content-type'
-};
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+// module.exports.cors = {
+//   allRoutes: true,
+//   origin: '*',
+//   credentials: true,
+//   methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+//   headers: 'content-type'
+// };
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//   })
+// );
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
