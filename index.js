e5a7d8b4daf414d62a5386c0287c6d70739be2af
MaheshChand1528 <mannu44800@gmail.com>
2022-09-02 21:23:02 +0530

    make it better

--- index.js
+++ index.js
@@ -8,17 +8,31 @@ var app = express();
 app.get("/", (req, res) => {
   res.send("server start");
 });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const DB = process.env.DATABASE;
 
 mongoose
