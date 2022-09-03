0d2e72c0b05a8a7928a17292b1a58f99adc9f86c
MaheshChand1528 <mannu44800@gmail.com>
2022-09-03 16:22:40 +0530

    backend

--- index.js
+++ index.js
@@ -9,39 +9,12 @@ app.get("/", (req, res) => {
   res.send("server start");
 });
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-// module.exports.cors = {
-//   allRoutes: true,
-//   origin: '*',
-//   credentials: true,
-//   methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
-//   headers: 'content-type'
-// };
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-//   })
-// );
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-const PORT = process.env.PORT || 5000;
-const DB = process.env.DATABASE;
+const PORT =5000;
+const DB = "mongodb+srv://admin:OcmGNASJxzXjndzx@cluster0.cwwdzqo.mongodb.net/Blog?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB)
